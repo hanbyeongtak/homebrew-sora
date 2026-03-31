702fa1cca572a2c7caae472d579610f88498a272
hanbyeongtak <bthan@maum.ai>
2026-03-31 16:27:59 +0900

    cc

---
+++ Formula/rms-cli.rb
@@ -0,0 +1,28 @@
+class RmsCli < Formula
+  desc "Maum Sora background service"
+  homepage "https://github.com/hanbyeongtak/rms_cli"
+  url "https://github.com/hanbyeongtak/rms_cli/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "02effcbd9026740b2487a9df6ffe2907d1997490fb9305a3da953608f2644aa4"
+  license "MIT"
+
+  def install
+    libexec.install Dir["*"]
+
+    (bin/"rms-cli").write <<~EOS
+      #!/usr/bin/env bash
+      export PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:$PATH"
+
+      APP_ROOT="$(brew --prefix maum-sora)/libexec"
+      cd "$APP_ROOT"
+
+      exec uv run python3 scripts/setup_rms.py "$@"
+    EOS
+
+    chmod 0755, bin/"rms-cli"
+  end
+
+
+  test do
+    system "#{bin}/maum-sora", "--help"
+  end
+end
